32c4138c268a0b792acfe2e77e271027288121b1
iffrit51 <bryan.charles51@hotmail.fr>
2021-10-15 17:04:03 +0200

    Ajout du check de photo pour l'instant

--- src/App.js
+++ src/App.js
@@ -9,6 +9,20 @@ const FEATURED_API = "https://api.themoviedb.org/3/discover/movie?sort_by=popula
 const SEARCH_API = "https://api.themovidb.org/3/search/movie&api_key=a738af5d7f7482f20997e6be8d8df8f9&query";
 
 
+
+const handleFileChange = event => {
+  var files = event.target.files;
+  var flattened = [];
+  for (let i = 0; i < files.length; i++) {
+    if (files[i].split('.').pop() == "jpg") {
+      flattened.push(files[i].name);
+      //list.innerHTML += '<br>' + file.webkitRelativePath
+    }
+  }
+  console.log(flattened);
+};
+
+
 function App() {
   const [movies, setMovies] = useState([]);
 
@@ -20,10 +34,14 @@ function App() {
       });
   }, []);
   return (
-  <div className="movie-container">
-    {movies.length > 0 && movies.map((movie) => 
+    <div className="movie-container">
+      <header>
+        <input id="picker" type="file" directory="" webkitdirectory=""
+          onChange={handleFileChange} />
+      </header>
+      {movies.length > 0 && movies.map((movie) =>
         <Movie key={movie.id} {...movie} />)}
-  </div>
+    </div>
   );
 
 }
