bb43ac3ced116ff522bd2cfdc5c90f74b92851fd
iffrit51 <bryan.charles51@hotmail.fr>
2021-10-18 15:15:56 +0200

    Add fileupload films

--- src/App.js
+++ src/App.js
@@ -1,47 +1,50 @@
-
 import Movie from './composants/Movie';
 
 import React, { useEffect, useState } from "react";
 
 const FEATURED_API = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=a738af5d7f7482f20997e6be8d8df8f9&page=1";
 
-
-const SEARCH_API = "https://api.themovidb.org/3/search/movie&api_key=a738af5d7f7482f20997e6be8d8df8f9&query";
-
-
-
-const handleFileChange = event => {
-  var files = event.target.files;
-  var flattened = [];
-  for (let i = 0; i < files.length; i++) {
-    if (files[i].split('.').pop() == "jpg") {
-      flattened.push(files[i].name);
-      //list.innerHTML += '<br>' + file.webkitRelativePath
-    }
-  }
-  console.log(flattened);
-};
-
+const SEARCH_API = "https://api.themoviedb.org/3/search/movie?api_key=a738af5d7f7482f20997e6be8d8df8f9&language=fr-FR&query=";
+var flattened = [];
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const handleFileChange = (e) => {
 
+    var files = e.target.files;
+
+
+    var textreplaced = "";
+
+    for (let i = 0; i < files.length - 5; i++) {
+      if (files[i].name.split('.').pop() === "mp4" || files[i].name.split('.').pop() === "avi" || files[i].name.split('.').pop() === "mkv") {
+        textreplaced = files[i].name.replace("_", " ");
+        textreplaced = textreplaced.replace("480", "");
+        textreplaced = textreplaced.replace("720", "");
+        textreplaced = textreplaced.replace("1080", "");
+        textreplaced = textreplaced.replace("cestpasbien", "").split('.').shift();
+        flattened.push(textreplaced);
+        console.log(SEARCH_API + textreplaced);
+        fetch(SEARCH_API + textreplaced).then(res => res.json())
+          .then(data => {
+            setMovies(data.results);
+          });
+      }
+    }
+  };
   useEffect(() => {
-    fetch(FEATURED_API).then(res => res.json())
-      .then(data => {
-        console.log(data);
-        setMovies(data.results);
-      });
   }, []);
   return (
-    <div className="movie-container">
+<>
       <header>
         <input id="picker" type="file" directory="" webkitdirectory=""
-          onChange={handleFileChange} />
+          onChange={handleFileChange} className="video" />
       </header>
+          <div className="movie-container">
       {movies.length > 0 && movies.map((movie) =>
         <Movie key={movie.id} {...movie} />)}
     </div>
+    </>
   );
 
 }
