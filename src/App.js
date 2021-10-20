e2ea02808a0aa7919ddd72be66d947c7c12edb06
iffrit51 <bryan.charles51@hotmail.fr>
2021-10-20 15:18:16 +0200

    Correction affichage image

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import Movie from './composants/Movie';
 
 import React, { useEffect, useState } from "react";
 
-const FEATURED_API = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=a738af5d7f7482f20997e6be8d8df8f9&page=1";
+//const FEATURED_API = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=a738af5d7f7482f20997e6be8d8df8f9&page=1";
 
 const SEARCH_API = "https://api.themoviedb.org/3/search/movie?api_key=a738af5d7f7482f20997e6be8d8df8f9&language=fr-FR&query=";
-var flattened = [];
-let flattenedTest = [];
+//var flattened;
+//var flattenedTest = [];
+var d = [];
 
 function App() {
   const [movies, setMovies] = useState([]);
@@ -14,36 +15,35 @@ function App() {
 
     var files = e.target.files;
 
+    d = fetchMovies(files);
+    setMovies(d);
+  };
 
+ function fetchMovies(files) {
     var textreplaced = "";
 
-    for (let i = 0; i < files.length; i++) {
+    for (let i = 0; i < files.length-1; i++) {
       if (files[i].name.split('.').pop() === "mp4" || files[i].name.split('.').pop() === "avi" || files[i].name.split('.').pop() === "mkv") {
         textreplaced = files[i].name.replace("_", " ").toLowerCase();
         textreplaced = textreplaced.split('.').shift();
         
-        console.log(SEARCH_API + textreplaced);
+        //console.log(SEARCH_API + textreplaced);
         fetch(SEARCH_API + textreplaced).then(res => res.json())
           .then((data) => {
+            
             if(data.results.length !== 0)
             {
-            flattened.push(data.results.slice(1,2));
-              if(flattenedTest.length<=1)
-              {
-                flattenedTest.push(flattened)
-                console.log(flattenedTest);
-                setMovies(flattenedTest);
-              }
+                d[i] = data.results.slice(1,2)[0];
+                //console.log(data.results);
+                console.log(d);
             }
-          },
-          (error) => {
-
-          }
-          );
+          },);
       }
-    }
-  };
+    } 
+    return d;
+  }
   useEffect(() => {
+    
   }, []);
   return (
 <>
